b1d11ed631aabf1db47f9bafd198400989982df7
Ror-05 <uietkuk236@gmail.com>
2026-04-06 23:54:56 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -53,7 +53,10 @@ export default function Home() {
         </div>
 
         <div className="flex items-center justify-center gap-2 m-5">
-          <input type="checkbox" className="accent-blue-500 w-4 h-4" />
+          <label htmlFor="check" className='flex bg-gray-100 cursor-pointer relative w-20 h-10 rounded-full'>
+            <input type="checkbox" id='check' className='sr-only-peer' />
+            <span className='w-2/5 h-4/5 bg-rose-300 absolute rounded-full left-1 top-1 peer-checked:bg-rose-600 peer-checked:left-11 transition-all duration-500'></span>
+          </label>
           <p className="text-sm text-gray-400">Auto paste from clipboard</p>
         </div>
 
